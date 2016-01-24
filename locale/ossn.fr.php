4d2d760fcb5218761dfe197c1dd72b1567f2b3d9
Eric <ctlui@users.noreply.github.com>
2016-01-24 09:22:26 +0100

    Add fr lang

---
+++ locale/ossn.fr.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$fr = array(
+   'htmlsidebar' => 'HTML dans la sidebar',
+   'htmlsidebar:saved' => 'Html sauvegardé avec succès',
+   'htmlsidebar:save:error' => 'Impossible de sauvegarder le code html',
+   'button:save' => 'Sauvegarder',
+   'htmlsidebar:entertext' => 'Entrer votre code HTML ici',
+);
+ossn_register_languages('fr', $fr);
